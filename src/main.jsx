c5d7ae5a1491e650e69b171d3b7477b013aaed65
nishant79901-beep <nishant79901@gmail.com>
2026-08-30 13:28:25 +0530

    fix: route production entry to verified application

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from './Entry2.jsx';createRoot(document.getElementById('root')).render(<App/>);
+import React from 'react';import{createRoot}from'react-dom/client';import App from './AppFixed.jsx';createRoot(document.getElementById('root')).render(<App/>);
